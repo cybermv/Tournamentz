5d1e4f335a8eceee5d40df2e12a911d266184aaa
Mateo Velenik <mateo.velenik@gmail.com>
2016-04-07 16:47:05 +0200

    added template host project

---
+++ Tournamentz.Host/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tournamentz.Host.MvcApplication" Language="C#" %>
---
+++ Tournamentz.Host/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tournamentz.Host.Startup))]
+namespace Tournamentz.Host
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
