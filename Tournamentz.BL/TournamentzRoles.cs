5c5cbe7a1d14abfa66de832b0a193c353382b26d
Mateo Velenik <mateo.velenik@gmail.com>
2016-05-02 00:00:48 +0200

    added stub views for tournament editing

--- Tournamentz.BL/TournamentzRoles.cs
+++ Tournamentz.BL/TournamentzRoles.cs
@@ -4,6 +4,9 @@
 
     public static class TournamentzRoles
     {
+        public const string AdminText = "Admin";
+        public const string UserText = "User";
+
         public const string Admin = "817EF2BA-3863-4C3A-954E-CBBA8809F361";
         public const string User = "3E2DDD01-0243-4E38-B563-80BCA9EF7BA7";
 
---
+++ Tournamentz.Host/Controllers/TournamentsController.cs
@@ -0,0 +1,49 @@
+﻿namespace Tournamentz.Host.Controllers
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web.Mvc;
+    using BL;
+    using BL.Core;
+    using BL.Core.Query.Interface;
+    using BL.Queries;
+    using Core;
+
+    [Authorize]
+    public class TournamentsController : TournamentzControllerBase
+    {
+        public TournamentsController(IExecutionContext executionContext)
+            : base(executionContext)
+        {
+        }
+
+        // GET: Tournaments
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return RedirectToAction("My");
+        }
+
+        // GET: Tournaments/My
+        [HttpGet]
+        public ActionResult My()
+        {
+            IQueryResult<TournamentQueries.My> myTournaments = this.RunQuery<TournamentQueries.My>();
+
+            List<TournamentQueries.My> myTournamentsList = myTournaments.Query.ToList();
+
+            return View(myTournamentsList);
+        }
+
+        // GET: Tournaments/All
+        [HttpGet, Authorize(Roles = TournamentzRoles.AdminText)]
+        public ActionResult All()
+        {
+            IQueryResult<TournamentQueries.All> allTournaments = this.RunQuery<TournamentQueries.All>();
+
+            List<TournamentQueries.All> allTournamentsList = allTournaments.Query.ToList();
+
+            return View(allTournamentsList);
+        }
+    }
+}
